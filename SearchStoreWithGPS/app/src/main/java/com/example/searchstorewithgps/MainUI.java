9bf42c503ad6253006b5b4daf4139409b489826f
2sjin <options3224@gmail.com>
2022-05-29 01:39:42 +0900

    임시 액티비티 MapActivity에 구글 맵 추가

--- SearchStoreWithGPS/app/src/main/java/com/example/searchstorewithgps/MainUI.java
+++ SearchStoreWithGPS/app/src/main/java/com/example/searchstorewithgps/MainUI.java
@@ -1,11 +1,11 @@
 package com.example.searchstorewithgps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainUI extends AppCompatActivity {
 
@@ -16,13 +16,17 @@ public class MainUI extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ui);
 
+        // 권한 설정
+
+
+
         startButton = findViewById(R.id.startButton);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), LocationSearchUI.class);
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivity(intent);
 
             }
