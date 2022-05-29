6d4b321bdfd8712a658f81c54726b921968a79cb
2sjin <options3224@gmail.com>
2022-05-29 22:47:36 +0900

    LocationSearchSys, LocationSearchUI 분리

--- SearchStoreWithGPS/app/src/main/java/com/example/searchstorewithgps/LocationSearchSys.java
+++ SearchStoreWithGPS/app/src/main/java/com/example/searchstorewithgps/LocationSearchSys.java
@@ -1,10 +1,42 @@
 package com.example.searchstorewithgps;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 public class LocationSearchSys {
     Store[] storeArray;
+    static LatLng deviceLocation = null;
+    private FusedLocationProviderClient fusedLocationClient;
 
-    public String getDeviceLocation() {
-        return "장치 위치";
+    public LatLng getDeviceLocation(Activity activity, Context context) {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity);
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+
+        Task a = fusedLocationClient.getLastLocation();
+        a.addOnSuccessListener(activity, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    deviceLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                }
+            }
+        });
+
+        return deviceLocation;
     }
 
     public String getStoreLocation() {
--- SearchStoreWithGPS/app/src/main/java/com/example/searchstorewithgps/LocationSearchUI.java
+++ SearchStoreWithGPS/app/src/main/java/com/example/searchstorewithgps/LocationSearchUI.java
@@ -3,7 +3,6 @@ package com.example.searchstorewithgps;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,16 +12,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.searchstorewithgps.databinding.ActivityMapsBinding;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 public class LocationSearchUI extends FragmentActivity implements
         GoogleMap.OnMyLocationButtonClickListener,
@@ -35,8 +30,6 @@ public class LocationSearchUI extends FragmentActivity implements
     private int MY_LOCATION_REQUEST_CODE = 1;
 
     static LatLng deviceLocation = null;
-
-    private FusedLocationProviderClient fusedLocationClient;
 
     public void openStoreCheckUI() {
     }
@@ -63,22 +56,6 @@ public class LocationSearchUI extends FragmentActivity implements
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), StoreCheckUI.class);
                 startActivity(intent);
-            }
-        });
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
-        Task a = fusedLocationClient.getLastLocation();
-        a.addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    deviceLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                }
             }
         });
     }
@@ -120,14 +97,18 @@ public class LocationSearchUI extends FragmentActivity implements
         mMap.setOnMyLocationButtonClickListener(this);
 
         // 현재 위치 마커 및 카메라 이동
-
-        searchButton.setText(String.valueOf(deviceLocation));
-
+        deviceLocation = ctrlSys.getDeviceLocation(this, this);
         if (deviceLocation != null) {
             mMap.addMarker(new MarkerOptions().position(deviceLocation).title("현재 위치"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(deviceLocation));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(deviceLocation, 18));
         }
+        else {
+            finish();
+            String msg = "위치 탐색 실패. 위치 서비스 확인 후 다시 시도하세요.";
+            Toast.makeText(this.getApplicationContext(),msg, Toast.LENGTH_SHORT).show();
+        }
+        searchButton.setText(String.valueOf(deviceLocation));
     }
 
     @Override
